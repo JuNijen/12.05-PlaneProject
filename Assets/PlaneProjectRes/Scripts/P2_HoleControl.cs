17079b6cbe0ae971876902ee468eeb1f3b780d57
JuS_Nijen <wjstldms0823@naver.com>
2016-12-22 21:12:46 +0900

    ADD PocketBall Project Init

---
+++ Assets/PlaneProjectRes/Scripts/P2_HoleControl.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P2_HoleControl : MonoBehaviour {
+
+    public MeshRenderer[] walls;
+
+    //float elaspedTime;
+
+    //void Update()
+    //{
+
+    //}
+
+
+
+
+    void OnTriggerEnter(Collider collider)
+    {
+        StartCoroutine(WallAnimation());
+    }
+
+    IEnumerator WallAnimation()
+    {
+        Color[] beforeColor = new Color[walls.Length];
+
+        for (int i = 0; i < beforeColor.Length; i++)
+        {
+            beforeColor[i] = walls[i].material.color;
+        }
+        SetColor(new Color(1, 1, 1));
+        yield return new WaitForSeconds(0.05f);
+        SetColor(new Color(0, 0, 0));
+        yield return new WaitForSeconds(0.05f);
+        SetColor(new Color(0.5f, 0.5f, 0.5f));
+        yield return new WaitForSeconds(0.05f);
+
+        for (int i = 0; i < beforeColor.Length; i++)
+        {
+            walls[i].material.color = beforeColor[i];
+        }
+
+    }
+
+    void SetColor(Color color)
+    {
+        for (int i = 0; i < walls.Length; i++)
+            walls[i].material.color = color;
+    }
+}
---
+++ Assets/PlaneProjectRes/Scripts/P2_PlayerControl.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class P2_PlayerControl : MonoBehaviour {
+
+    public BoxCollider floor;
+
+	void Update () {
+        if (Input.GetMouseButtonUp(0) == true)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+
+            if (floor.Raycast(ray, out hitInfo, 1000) == true)
+            {
+                Vector3 myPos = transform.position;
+                Vector3 hitPos = hitInfo.point;
+
+                myPos.y = 0;
+                hitPos.y = 0;
+
+                Vector3 dir = hitPos - myPos;
+                GetComponent<Rigidbody>().AddForce(dir * 5.0f, ForceMode.Impulse);
+
+            }
+        }
+	}
+}
